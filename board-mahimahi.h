c127b7dbf3b178ec489085e21a91c838f49f45b4
Prashant Somashekar <prashsomash@gmail.com>
2010-05-04 21:55:01 -0400

    Eliminated third memory bank in EBI1_BANK0 and added that memory into the first memory bank (coolbho)

--- board-mahimahi.h
+++ board-mahimahi.h
@@ -21,6 +21,14 @@
 #define MSM_SMI_BASE		0x02B00000
 #define MSM_SMI_SIZE		0x01500000
 
+/* Camera PMEM has been moved here from EBI1 */
+#define MSM_PMEM_CAMERA_BASE    0x02B00000
+#define MSM_PMEM_CAMERA_SIZE    0x00800000
+
+/* GPU MEM has been moved here from EBI1 */
+#define MSM_GPU_MEM_BASE        0x03300000
+#define MSM_GPU_MEM_SIZE        0x00500000
+
 #define MSM_RAM_CONSOLE_BASE	0x03A00000
 #define MSM_RAM_CONSOLE_SIZE	0x00040000
 
@@ -31,14 +39,6 @@
 #define MSM_EBI1_BANK0_BASE     0x20000000
 #define MSM_EBI1_BANK0_SIZE     0x0E800000
 
-/* Extra memory bank in place of GPU MEM */
-#define MSM_XTRAMEM_BASE        0x2DB00000
-#define MSM_XTRAMEM_SIZE        0x00D00000
-
-/* GPU MEM has been moved here from EBI1 */
-#define MSM_GPU_MEM_BASE        0x03300000
-#define MSM_GPU_MEM_SIZE        0x00500000
-
 #define MSM_EBI1_BANK1_BASE	0x30000000
 #define MSM_EBI1_BANK1_SIZE	0x10000000
 
@@ -48,13 +48,8 @@
 #define MSM_PMEM_ADSP_BASE	0x32000000
 #define MSM_PMEM_ADSP_SIZE	0x02900000
 
-/* Camera PMEM is back */
-#define MSM_PMEM_CAMERA_BASE    0x02B00000
-#define MSM_PMEM_CAMERA_SIZE    0x00800000
-
-/* Unnecessary camera PMEM removed to add to highmem */
-#define MSM_HIGHMEM_BASE 0x34900000
-#define MSM_HIGHMEM_SIZE 0x0B700000
+#define MSM_HIGHMEM_BASE        0x34900000
+#define MSM_HIGHMEM_SIZE        0x0B700000
 
 #define MAHIMAHI_GPIO_PS_HOLD		25
 
